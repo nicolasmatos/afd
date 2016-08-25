e470d38f7a86240783a450d6ac416665dae0edb0
Ruan Nícolas <nicolasmatos0905@gmail.com>
2016-08-25 15:15:24 -0300

    Comentários

--- src/afd/Automato.java
+++ src/afd/Automato.java
@@ -5,7 +5,6 @@
  */
 package afd;
 
-
 import java.util.ArrayList;
 
 public class Automato {
@@ -31,7 +30,7 @@ public class Automato {
         
         eIvalido = false;
         saida = 0;
-        entrada = new ArrayList<String>();
+        entrada = new ArrayList<>();
     }
     
     public Automato(){
@@ -40,14 +39,6 @@ public class Automato {
 
     public String verificacao() {
         String resultado = "";
-        /*Estado estadoAtual;
-        String txtAlfabeto = "+,-,0,1,2,3,4,5,6,7,8,9";
-        int qtdEstados = 4, saida = 0;
-        String txtTransicoes = "1/+/2,1/-/2,1/0/4,1/1/3,1/2/3,1/3/3,1/4/3,1/5/3,1/6/3,1/7/3,1/8/3,1/9/3,2/0/3,2/1/3,2/2/3,2/3/3,2/4/3,2/5/3,2/6/3,2/7/3,2/8/3,2/9/3,3/0/3,3/1/3,3/2/3,3/3/3,3/4/3,3/5/3,3/6/3,3/7/3,3/8/3,3/9/3";
-        String txtInicial = "1";
-        String txtFinais = "3,4";
-        String txtEntrada = "+/0/-/3";
-        ArrayList<String> entrada = new ArrayList<String>();*/
 
         String[] alfabeto = txtAlfabeto.split(",");
         Estado[] estados = new Estado[qtdEstados + 1];
